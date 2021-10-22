4f248bef13fafdb9dc6fc316500195887151d12e
SladkoewkaX <Bapaev2000@gmail.com>
2021-10-22 19:41:17 +0600

    third fix

--- for_t/models.py
+++ for_t/models.py
@@ -5,4 +5,4 @@ class Registration(models.Model):
     surname = models.CharField(max_length=30)
     
     def __str__(self):
-        return self.surname
+        return f'{self.surname}, {self.name}'
